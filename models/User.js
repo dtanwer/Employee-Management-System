5bce956c3c75bc1d5effb33dcfdf15af462b82f2
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-09-14 11:21:20 +0530

    we

--- models/User.js
+++ models/User.js
@@ -1,33 +1,31 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type:String,
-        required:true,
+    phone: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'is invalid Email'],
+    email: {
+      type: String,
+      required: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "is invalid Email"],
     },
-    password:{
-        type:String,
-        default:'123'
+    password: {
+      type: String,
+      default: "123",
     },
-    userType:{
-        type:String,
-        required:true,
-        enum:["admin","techlead","user"]
-    }
-},
-{timestamps:true}
-)
+    userType: {
+      type: String,
+      required: true,
+      enum: ["admin", "techlead", "user"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports=mongoose.model('User',UserSchema)
-
-
-
+module.exports = mongoose.model("User", UserSchema);
