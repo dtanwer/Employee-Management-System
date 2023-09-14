81da1d5a272b5a36535771e4c25dcb14cc3fa2e6
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-09-14 17:51:10 +0530

    drsg

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const express = require('express');
-const announcementRouter = require('./routes/announcement');
-const authRouter = require('./routes/auth');
-const feedbackRouter = require('./routes/feedback');
-const issueRouter = require('./routes/issue');
-const leaveRouter = require('./routes/leave');
-const projectIdeasRouter = require('./routes/projectIdea');
-const userRouter = require('./routes/user');
+const express = require("express");
+const announcementRouter = require("./routes/announcement");
+const authRouter = require("./routes/auth");
+const feedbackRouter = require("./routes/feedback");
+const issueRouter = require("./routes/issue");
+const leaveRouter = require("./routes/leave");
+const projectIdeasRouter = require("./routes/projectIdea");
+const userRouter = require("./routes/user");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("Api is working");
+app.get("/", (req, res) => {
+  res.send("Api is working");
 });
 
-app.use('/user',userRouter)
-app.use('/auth',authRouter)
-app.use('/announcement',announcementRouter)
-app.use('/feedback',feedbackRouter)
-app.use('/issue',issueRouter)
-app.use('/leave',leaveRouter)
-app.use('/projectIdea',projectIdeasRouter)
-
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/announcement", announcementRouter);
+app.use("/feedback", feedbackRouter);
+app.use("/issue", issueRouter);
+app.use("/leave", leaveRouter);
+app.use("/projectIdea", projectIdeasRouter);
 
 module.exports = app;
